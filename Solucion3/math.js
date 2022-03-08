283063750dae2cfaedf1202c4402b283515c2e71
DANI MI <zeleugim@gmail.com>
2022-03-08 18:35:10 +0100

    2022/03/08

--- Solucion3/math.js
+++ Solucion3/math.js
@@ -1,4 +1,6 @@
 const data = require('./data.json')
+const a = data.a;
+const b = data.b;
 
 class Math1 {
     static sum(a, b) { `Sum of ${a} and ${b} is ${a+b}`};
@@ -6,24 +8,24 @@ class Math1 {
     static mult(a, b) { `Multiplication of ${a} and ${b} is ${a * b }` }
 }
 
-
 //middleware
 class Math2 {
-    square (a,b,next) {
+    static square (next) {
         console.log(`Square of ${a} is ${a**2}`);
         console.log(`Square of ${b} is ${b**2}`);
-        //next();
+        next();
     }
-    cube (a,b,next) {
+    static cube (next) {
         console.log(`Cube of ${a} is ${a**3}`);
         console.log(`Cube of ${b} is ${b**3}`);
-        //next();
+        next();
     }
-    div (a,b,next) {
-        console.log(`${a/b} is the result of dividing ${a} by ${b}`);
-        //next();
+    static div (next) {
+        console.log(`${(a/b)} is the result of dividing ${a} by ${b}`);
+        next();
     }
 }
 
 module.exports = {Math1, Math2};
+Math2.square()
 
