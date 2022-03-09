eac841806ca7eccf366f1ee9e44df6ec8e2d4a33
DANI MI <zeleugim@gmail.com>
2022-03-09 17:21:47 +0100

    2022/03/09

--- Decorator/Solucion/app.js
+++ Decorator/Solucion/app.js
@@ -1,8 +1,18 @@
 const items = require('./items.json')
+const currencies = require('./currency_conversions.json')
 const decorate = require('./decorator.js')
 
+
+//Original prices
+console.log("<<<<<<<<<<<<ORIGINAL ITEMS WITH LOCAL CURRENCY>>>>>>>>>>>")
 for (let i = 0; i < items.length; i++) {
-    decorate(items[i])
+    console.log(items[i])
+}
+
+//Prices in Euros after decorating
+console.log("<<<<<<<<<<<ITEMS WITH PRICES IN EUROS>>>>>>>>>>>>>><")
+for (let i = 0; i < items.length; i++) {
+    decorate(items[i],currencies)
 }
 
 
